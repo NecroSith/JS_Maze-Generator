1f2e6968c5613eb576386c50b294fc67df5ce158
Yan_Pustynnyy <necrosith@yandex.ru>
2019-09-21 23:27:24 +0300

    Added tracker. Added functions to move tracker and to not move into the walls

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 'use strict';
 
-const COLUMN_COUNT = 10;
-const ROW_COUNT = 10;
-const CELL_SIZE = 50;
-const PADDING = 10;
+const COLUMN_COUNT = 10,
+    ROW_COUNT = 10,
+    CELL_SIZE = 50,
+    PADDING = 10,
+    WALL_COLOR = 'black',
+    SPACE_COLOR = 'white';
 
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
-const map = createMap();
+const canvas = document.querySelector('canvas'),
+    context = canvas.getContext('2d'),
+    map = createMap(),
+    tracker = { x: 0, y: 0 };
 
 init();
+console.log(isWall(2, 2))
 
 function init() {
     canvas.width = PADDING * 2 + COLUMN_COUNT * CELL_SIZE;
@@ -23,31 +27,114 @@ function start() {
 
 function tick(timestamp) {
     clearCanvas();
+    drawMap();
+    drawTracker();
+    moveTracker();
     requestAnimationFrame(tick);
 }
 
 function clearCanvas() {
-    context.fillStyle = 'black';
-    context.beginPath();
-    context.rect(0, 0, canvas.width, canvas.height);
-    context.fill();
-
-    context.fillStyle = 'white';
-    context.beginPath();
-    context.rect(PADDING, PADDING, canvas.width - PADDING * 2, canvas.height - PADDING * 2);
-    context.fill();
+    drawRectangle(WALL_COLOR, 0, 0, canvas.width, canvas.height);
+    drawRectangle(SPACE_COLOR, PADDING, PADDING, canvas.width - PADDING * 2, canvas.height - PADDING * 2);
 }
 
 function createMap() {
     let map = [];
-
     for (let i = 0; i < COLUMN_COUNT; i++) {
-        let column = [];
+        let columns = [];
         for (let j = 0; j < ROW_COUNT; j++) {
-            column.push('wall');
+            columns.push('wall');
         }
-        map.push(column);
+        map.push(columns);
+    }
+    return map;
+}
+
+function drawMap() {
+    for (let i = 0; i < COLUMN_COUNT; i++) {
+        for (let j = 0; j < ROW_COUNT; j++) {
+            drawRectangle(WALL_COLOR, PADDING + i * CELL_SIZE, PADDING + j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+        }
+    }
+}
+
+function drawTracker() {
+    drawRectangle('red', PADDING + tracker.x * CELL_SIZE, PADDING + tracker.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+}
+
+function moveTracker() {
+    let directions = ['left', 'right', 'up', 'down'];
+
+    if (tracker.x <= 0) {
+        directions = [...directions.slice(0, 'left'), ...directions.slice(directions.indexOf('left') + 1)];
+    }
+    if (tracker.x >= COLUMN_COUNT - 1) {
+        directions = [...directions.slice(0, 'right'), ...directions.slice(directions.indexOf('right') + 1)];
+    }
+    if (tracker.y <= 0) {
+        directions = [...directions.slice(0, 'up'), ...directions.slice(directions.indexOf('up') + 1)];
+    }
+    if (tracker.y >= ROW_COUNT - 1) {
+        directions = [...directions.slice(0, 'down'), ...directions.slice(directions.indexOf('down') + 1)];
     }
 
-    return map;
+    const direction = getRandomFrom(directions);
+    console.log(direction);
+
+
+    switch (direction) {
+        case 'left':
+            if (isWall(tracker.x - 2, tracker.y)) {
+                setSpace(tracker.x - 1, tracker.y);
+                setSpace(tracker.x - 2, tracker.y);
+            }
+            tracker.x -= 2;
+            break;
+        case 'right':
+            if (isWall(tracker.x + 2, tracker.y)) {
+                setSpace(tracker.x + 1, tracker.y);
+                setSpace(tracker.x + 2, tracker.y);
+            }
+            tracker.x += 2;
+            break;
+        case 'up':
+            if (isWall(tracker.x, tracker.y - 2)) {
+                setSpace(tracker.x, tracker.y - 1);
+                setSpace(tracker.x, tracker.y - 2);
+            }
+            tracker.y -= 2;
+            break;
+        case 'down':
+            if (isWall(tracker.x, tracker.y + 2)) {
+                setSpace(tracker.x, tracker.y + 1);
+                setSpace(tracker.x, tracker.y + 2);
+            }
+            tracker.y += 2;
+            break;
+    }
+}
+
+function getRandomFrom(input) {
+    return input[Math.floor(Math.random() * input.length - 1) + 1];
+}
+
+function isWall(x, y) {
+    console.log(x + ": " + y);
+    if (map[x][y] === 'wall') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function setSpace(x, y) {
+    drawRectangle(SPACE_COLOR, x, y, CELL_SIZE, CELL_SIZE);
+    return map[x][y] = 'space';
+}
+
+function drawRectangle(fillColor, startX, startY, endX, endY) {
+    context.fillStyle = fillColor;
+    context.beginPath();
+    context.rect(startX, startY, endX, endY);
+    context.fill();
 }
